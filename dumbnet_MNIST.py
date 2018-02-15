08f37f6a30a358e7a541122cca89c17c38530630
Pascal Stammer <pstammer@agile-im.de>
2018-02-15 07:31:53 +0100

    do some basic restructure

--- dumbnet_MNIST.py
+++ dumbnet_MNIST.py
@@ -4,6 +4,7 @@ from helper import MNIST
 from matplotlib import pyplot as plt
 from layers import feed_forward_layer, conv_layer
 
+
 def get_siamnese(X):
     """
     creates the siamnese stem for the neural network. This is the part both images are send through sequentially
@@ -11,46 +12,47 @@ def get_siamnese(X):
     :return:
     """
     with tf.variable_scope('Layer1'):
-        conv1 = conv_layer(X,16,5,5,activation_function=tf.nn.relu)
+        conv1 = conv_layer(X, 16, 5, 5, activation_function=tf.nn.relu)
 
     with tf.variable_scope('Layer2'):
-        conv2 = conv_layer(conv1,32,3,3,activation_function=tf.nn.relu)
+        conv2 = conv_layer(conv1, 32, 3, 3, activation_function=tf.nn.relu)
 
     with tf.variable_scope('Layer4'):
-       conv3 = conv_layer(conv2,128,2,2,activation_function=tf.nn.relu)
+        conv3 = conv_layer(conv2, 128, 2, 2, activation_function=tf.nn.relu)
 
     flat = tf.layers.flatten(conv3)
     return flat
 
+
 # Define the Network Graph
 with tf.variable_scope('Input'):
-    X1 = tf.placeholder(tf.float32,shape=[None,28,28,1],name='Img1')
-    #X2 = tf.placeholder(tf.float32,shape=[None,28,28,1],name='Img2')
-    Y = tf.placeholder(tf.float32, shape=[None,10],name='Labels')
+    X1 = tf.placeholder(tf.float32, shape=[None, 28, 28, 1], name='Img1')
+    # X2 = tf.placeholder(tf.float32,shape=[None,28,28,1],name='Img2')
+    Y = tf.placeholder(tf.float32, shape=[None, 10], name='Labels')
 
 with tf.variable_scope('Siamese_Segment') as scope:
     left_net = get_siamnese(X1)
-    #scope.reuse_variables()
-    #right_net = get_siamnese(X2)
+    # scope.reuse_variables()
+    # right_net = get_siamnese(X2)
 
 with tf.variable_scope('Dense_Segment'):
     concatenated = left_net
-    d1 = feed_forward_layer(concatenated,1024,activation_function=tf.nn.relu)#dense(concatenated,4096*2,4096,tf.nn.relu,'Dense1')
+    d1 = feed_forward_layer(concatenated, 1024,
+                            activation_function=tf.nn.relu)  # dense(concatenated,4096*2,4096,tf.nn.relu,'Dense1')
 
 # logits have their own scope to avoid variable dublication
 with tf.variable_scope('Logit'):
-    logits = feed_forward_layer(d1,10)
-
+    logits = feed_forward_layer(d1, 10)
 
 with tf.variable_scope('Loss_Metrics_and_Training'):
     # use weighted loss, since images of two different classes statically outnumber the matches by 10:1
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y,logits=logits))
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=logits))
 
     # calculate the accuracy
-    predicted_class = tf.argmax(logits,1)
-    ground_truth = tf.argmax(Y,1)
-    correct = tf.equal(predicted_class,ground_truth)
-    accuracy = tf.reduce_mean( tf.cast(correct, 'float') )
+    predicted_class = tf.argmax(logits, 1)
+    ground_truth = tf.argmax(Y, 1)
+    correct = tf.equal(predicted_class, ground_truth)
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
     # use GradientDecent to train, interestingly ADAM results in a collapsing model. Standard SGD performed reliably better
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
@@ -66,11 +68,13 @@ acc_list = []
 l = 0.0
 init = tf.global_variables_initializer()
 
+
 def get_one_hot(labels):
-    oh_labels = np.zeros((len(labels),10))
+    oh_labels = np.zeros((len(labels), 10))
     for i in range(len(labels)):
         oh_labels[i][labels[i]] = 1
     return oh_labels
+
 
 with tf.Session() as sess:
     sess.run(init)
@@ -78,48 +82,44 @@ with tf.Session() as sess:
     v_acc = []
     for epoch in range(epochs):
         m = MNIST('.')
-        print('[INFO] Epoch:',epoch,l)
+        print('[INFO] Epoch:', epoch, l)
         i = 0
         for data, labels in m.get_training_batch(batch_size):
             # reshape data to fit the tf-standard shapes for images
-            data = data.reshape((data.shape[0],28,28,1))
+            data = data.reshape((data.shape[0], 28, 28, 1))
             labels = get_one_hot(labels)
 
-
             # run training step
-            _,l = sess.run([train_step,loss],feed_dict={X1:data,Y:labels})
+            _, l = sess.run([train_step, loss], feed_dict={X1: data, Y: labels})
             loss_list.append(l)
 
-
             # test model on validation data
-            if i%100 == 0:
+            if i % 100 == 0:
                 v_ll = []
                 v_ac = []
 
                 for data, labels in m.get_validation_batch(batch_size):
-
                     # same code as in the training loop
-                    data = data.reshape((data.shape[0],28,28,1))
+                    data = data.reshape((data.shape[0], 28, 28, 1))
                     labels = get_one_hot(labels)
 
-                    pc,l,a = sess.run([predicted_class,loss,accuracy],feed_dict={X1:data,Y:labels})
+                    pc, l, a = sess.run([predicted_class, loss, accuracy], feed_dict={X1: data, Y: labels})
 
                     # keep track of loss and accuracy
                     v_ll.append(l)
                     v_ac.append(a)
-            v_loss.append([np.mean(v_ll)]*100)
-            v_acc.append([np.mean(v_ac)]*100)
+            v_loss.append([np.mean(v_ll)] * 100)
+            v_acc.append([np.mean(v_ac)] * 100)
             i += 1
 
-
     # plot the loss curve.
-    plt.plot(list(range(len(loss_list))),loss_list, label='Training Loss')
-    plt.plot(list(range(len(loss_list))),loss_list, label='Validation Loss')
+    plt.plot(list(range(len(loss_list))), loss_list, label='Training Loss')
+    plt.plot(list(range(len(loss_list))), loss_list, label='Validation Loss')
     plt.legend()
     plt.show()
 
     # print the mena accuracy over the test-set
-    print('Val Acc: ',np.mean(v_acc))
+    print('Val Acc: ', np.mean(v_acc))
     print('Val Loss: ', np.mean(v_loss))
 
     # test model on validation data
@@ -127,16 +127,15 @@ with tf.Session() as sess:
     acc_list = []
     l = 0.0
     for data, labels in m.get_test_batch(batch_size):
-
         # same code as in the training loop
-        data = data.reshape((data.shape[0],28,28,1))
+        data = data.reshape((data.shape[0], 28, 28, 1))
         labels = get_one_hot(labels)
 
-        pc,l,a = sess.run([predicted_class,loss,accuracy],feed_dict={X1:data,Y:labels})
+        pc, l, a = sess.run([predicted_class, loss, accuracy], feed_dict={X1: data, Y: labels})
 
         # keep track of loss and accuracy
         loss_list.append(l)
         acc_list.append(a)
 
     # print the mena accuracy over the test-set
-    print('Test Acc:',np.mean(acc_list))
+    print('Test Acc:', np.mean(acc_list))
--- simnet_MNIST.py
+++ simnet_MNIST.py
@@ -4,6 +4,7 @@ from helper import MNIST
 from matplotlib import pyplot as plt
 from layers import feed_forward_layer, conv_layer
 
+
 def get_siamnese(X):
     """
     creates the siamnese stem for the neural network. This is the part both images are send through sequentially
@@ -11,22 +12,23 @@ def get_siamnese(X):
     :return:
     """
     with tf.variable_scope('Layer1'):
-        conv1 = conv_layer(X,16,5,5,activation_function=tf.nn.relu)
+        conv1 = conv_layer(X, 16, 5, 5, activation_function=tf.nn.relu)
 
     with tf.variable_scope('Layer2'):
-        conv2 = conv_layer(conv1,32,3,3,activation_function=tf.nn.relu)
+        conv2 = conv_layer(conv1, 32, 3, 3, activation_function=tf.nn.relu)
 
     with tf.variable_scope('Layer4'):
-       conv3 = conv_layer(conv2,128,2,2,activation_function=tf.nn.relu)
+        conv3 = conv_layer(conv2, 128, 2, 2, activation_function=tf.nn.relu)
 
     flat = tf.layers.flatten(conv3)
     return flat
 
+
 # Define the Network Graph
 with tf.variable_scope('Input'):
-    X1 = tf.placeholder(tf.float32,shape=[None,28,28,1],name='Img1')
-    X2 = tf.placeholder(tf.float32,shape=[None,28,28,1],name='Img2')
-    Y = tf.placeholder(tf.float32, shape=[None,1],name='Labels')
+    X1 = tf.placeholder(tf.float32, shape=[None, 28, 28, 1], name='Img1')
+    X2 = tf.placeholder(tf.float32, shape=[None, 28, 28, 1], name='Img2')
+    Y = tf.placeholder(tf.float32, shape=[None, 1], name='Labels')
 
 with tf.variable_scope('Siamese_Segment') as scope:
     left_net = get_siamnese(X1)
@@ -34,23 +36,23 @@ with tf.variable_scope('Siamese_Segment') as scope:
     right_net = get_siamnese(X2)
 
 with tf.variable_scope('Dense_Segment'):
-    concatenated = tf.concat([left_net,right_net],axis=1,name='concatenated_out')
-    d1 = feed_forward_layer(concatenated,1024,activation_function=tf.nn.relu)#dense(concatenated,4096*2,4096,tf.nn.relu,'Dense1')
+    concatenated = tf.concat([left_net, right_net], axis=1, name='concatenated_out')
+    d1 = feed_forward_layer(concatenated, 1024,
+                            activation_function=tf.nn.relu)  # dense(concatenated,4096*2,4096,tf.nn.relu,'Dense1')
 
 # logits have their own scope to avoid variable dublication
 with tf.variable_scope('Logit'):
-    logits = feed_forward_layer(d1,1)
-
+    logits = feed_forward_layer(d1, 1)
 
 with tf.variable_scope('Loss_Metrics_and_Training'):
     # use weighted loss, since images of two different classes statically outnumber the matches by 10:1
-    loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(Y,logits,0.1,'Loss'))
+    loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(Y, logits, 0.1, 'Loss'))
 
     # calculate the accuracy
-    predicted_class = tf.greater(tf.nn.sigmoid(logits),0.5)
+    predicted_class = tf.greater(tf.nn.sigmoid(logits), 0.5)
     sigmoidal_out = tf.nn.sigmoid(logits)
-    correct = tf.equal(predicted_class, tf.equal(Y,1.0))
-    accuracy = tf.reduce_mean( tf.cast(correct, 'float') )
+    correct = tf.equal(predicted_class, tf.equal(Y, 1.0))
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
     # use GradientDecent to train, interestingly ADAM results in a collapsing model. Standard SGD performed reliably better
     train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
@@ -72,16 +74,16 @@ with tf.Session() as sess:
     i = 0
     for epoch in range(epochs):
         m = MNIST('.')
-        print('[INFO] Epoch:',epoch,l)
+        print('[INFO] Epoch:', epoch, l)
         for data, labels in m.get_training_batch(batch_size):
             # reshape data to fit the tf-standard shapes for images
-            data = data.reshape((data.shape[0],28,28,1))
+            data = data.reshape((data.shape[0], 28, 28, 1))
 
             # use a batch of data from the Data Provider and reshape it to suit the needs of the siamnese network
             # the first half of the batch is matched with the second half and the labels are calculated accordingly
             x1 = data[:half_batch]
             x2 = data[half_batch:]
-            y = np.zeros((half_batch,1))
+            y = np.zeros((half_batch, 1))
             y1 = labels[:half_batch]
             y2 = labels[half_batch:]
 
@@ -90,31 +92,30 @@ with tf.Session() as sess:
             y[y1 != y2] = 1.0
 
             # run training step
-            _,l = sess.run([train_step,loss],feed_dict={X1:x1,X2:x2,Y:y})
+            _, l = sess.run([train_step, loss], feed_dict={X1: x1, X2: x2, Y: y})
             loss_list.append(l)
 
-            if i%100 == 0:
+            if i % 100 == 0:
                 v_loss = []
                 v_acc = []
                 # test model on validation data
                 for data, labels in m.get_validation_batch(batch_size):
-
                     # same code as in the training loop
-                    data = data.reshape((data.shape[0],28,28,1))
+                    data = data.reshape((data.shape[0], 28, 28, 1))
                     x1 = data[:half_batch]
                     x2 = data[half_batch:]
-                    y = np.zeros((half_batch,1))
+                    y = np.zeros((half_batch, 1))
                     y1 = labels[:half_batch]
                     y2 = labels[half_batch:]
                     y[y1 == y2] = 0.0
                     y[y1 != y2] = 1.0
-                    pc,l,a = sess.run([predicted_class,loss,accuracy],feed_dict={X1:x1,X2:x2,Y:y})
+                    pc, l, a = sess.run([predicted_class, loss, accuracy], feed_dict={X1: x1, X2: x2, Y: y})
 
                     # keep track of loss and accuracy
                     v_loss.append(l)
                     v_acc.append(a)
-                v_loss_list += [np.mean(v_loss)]*100
-                v_acc_list += [np.mean(v_acc)]*100
+                v_loss_list += [np.mean(v_loss)] * 100
+                v_acc_list += [np.mean(v_acc)] * 100
             i += 1
 
     # print the mean accuracy over the test-set
@@ -126,14 +127,15 @@ with tf.Session() as sess:
     # set is highly unbalanced in favour of missmatches.
     for i in range(len(x1)):
         if not pc[i]:
-            print(y1[i],y2[i],'Same')
+            print(y1[i], y2[i], 'Same')
 
     # plot the loss curve.
-    plt.plot(list(range(len(loss_list))),loss_list, label='Train Loss')
-    plt.plot(list(range(len(loss_list))),v_loss_list, label='Val Loss')
+    plt.plot(list(range(len(loss_list))), loss_list, label='Train Loss')
+    plt.plot(list(range(len(loss_list))), v_loss_list, label='Val Loss')
     plt.show()
-    plt.plot(list(range(len(loss_list))),v_acc_list, label='Validation Accuracy')
+    plt.plot(list(range(len(loss_list))), v_acc_list, label='Validation Accuracy')
     plt.show()
+
 
     def get_mean_prediction(predictions, y):
         score_map = []
@@ -143,6 +145,7 @@ with tf.Session() as sess:
             score_map.append(m)
         return np.argmin(score_map)
 
+
     # CLASSIFICATION SEGMENT
     test_acc = []
     samples_per_shot = 100
@@ -150,24 +153,24 @@ with tf.Session() as sess:
     correct = 0.0
     correct_avg = 0.0
     for data, labels in m.get_test_batch(batch_size):
-        data = data.reshape((data.shape[0],28,28,1))
-        print('[INFO] processing',total_data_processed,'of',m.get_sizes()[2])
+        data = data.reshape((data.shape[0], 28, 28, 1))
+        print('[INFO] processing', total_data_processed, 'of', m.get_sizes()[2])
 
-        #calssify a single sample
+        # calssify a single sample
         for i in range(len(data)):
             x1, y1 = m.get_classification_samples(samples_per_shot // 10)
             x2 = np.asarray([list(data[i])] * samples_per_shot)
 
-            pc = sess.run([sigmoidal_out],feed_dict={X1:x1,X2:x2})
+            pc = sess.run([sigmoidal_out], feed_dict={X1: x1, X2: x2})
             prediction = y1[np.argmin(pc)]
-            prediction_avg = get_mean_prediction(np.squeeze(pc),y1)
+            prediction_avg = get_mean_prediction(np.squeeze(pc), y1)
 
             if prediction == labels[i]:
                 correct += 1.0
             if prediction_avg == labels[i]:
                 correct_avg += 1.0
             total_data_processed += 1.0
-        # keep track of loss and accuracy
+            # keep track of loss and accuracy
     accuracy = correct / total_data_processed
     avg_acc = correct_avg / total_data_processed
     print('Classification Accuracy:  ', accuracy)
